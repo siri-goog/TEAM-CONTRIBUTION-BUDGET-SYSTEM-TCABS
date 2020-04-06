62dac5017a02155931e3abcfe90576b00b50ba6b
siri-goog <62740091+siri-goog@users.noreply.github.com>
2020-04-06 16:45:51 +1000

    edit student management

--- Admin_StuManagement.aspx.vb
+++ Admin_StuManagement.aspx.vb
@@ -28,15 +28,6 @@
             chk = 0
         End If
 
-
-        'sql(0) = "select comcode from master_company where comcode = '" & me.lblcomcode.text & "' "
-        'dt = m1.getdatatable(sql(0))
-        'if dt.rows.count > 0 then
-        '    alert("รหัสบริษัทนี้ได้รับการเพิ่มแล้วค่ะ\nthis company code is already exist")
-        '    chk = 0
-        'end if
-
-
         If chk = 0 Then
             Return False
         Else
@@ -49,21 +40,16 @@
 #Region "Manage Student"
 
     '--load gridview
-    'Sub loaddata()
-    '    Sql(0) = " select * , case when a.status = 'y' then 'use' else 'not use' end as status1 ," _
-    '           & " case when a.holding_group = 'aa' then 'aa : paper holding' when a.holding_group = 'po' then 'po : power holding'  else 'qs : other holding' end as holding , " _
-    '           & " company_code + ' , ' + b.company_namet as companyname from [true].[dbo].[master_company] a" _
-    '           & " join [hrms].[dbo].[com_company] b on a.id_company = b.id_company " _
-    '           & " order by holding_group asc, comcode asc "
-    '    dt = m1.getdatatable(Sql(0))
-    '    gvStudent.DataSource = dt
-    '    gvStudent.DataBind()
-    'End Sub
+    Sub loaddata()
+        SQL(0) = " select * From Student "
+        DT = M1.GetDatatable(SQL(0))
+        gvStudent.DataSource = DT
+        gvStudent.DataBind()
+    End Sub
 
-    '--แสดงข้อมูลทุกครั้งที่ load page ขึ้นมา
     Protected Sub page_load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not Page.IsPostBack Then
-            'loaddata()
+            loaddata()
         End If
     End Sub
 
@@ -84,160 +70,99 @@
         Dim stuName As String = Trim(Me.txtStuName.Text)
         Dim stuLevel As String = ddlStudentLevel.SelectedValue.ToString()
 
-        SQL(0) = (" execute ADD_STUDENT @stuid = '" & stuID & "', @stuname = '" & stuName & "', @stulevel = '" & stuLevel & "' ")
+        SQL(0) = ("ADD_STUDENT @stuID, @stuName, @stuLevel")
+        'SQL(0) = "Insert into Student (stuID, stuName, stuLevel) " _
+        '& " values (" & stuID & ", '" & stuName & "', '" & stuLevel & "') "
+
         M1.Execute(SQL(0))
-        'alert("Data is complete.")
-        'gvStudent.EditIndex = -1
-        'loaddata()
-
-        'Dim sqlquery As String = ("execute add_student @stuid = " & stuID & ", @stuname = '" & stuName & "', @stulevel = '" & stuLevel & "' ")
-        'Dim cmd As New sqlcommand(sqlquery, sqlconn)
-        'Try
-        '    sqlconn.open()
-        '    Dim changes As Integer = cmd.executenonquery()
-        '    sqlconn.close()
-        '    messagebox.show(changes & "record deleted")
-
-        'Catch ex As Exception
-        '    messagebox.show(ex.Message)
-        'End Try
-
-        txtStuID.Text = ""
-        txtStuName.Text = ""
-        ddlStudentLevel.SelectedIndex() = 0
+        Try
+            alert("Data entered successfully.")
+            txtStuID.Text = ""
+            txtStuName.Text = ""
+            ddlStudentLevel.SelectedIndex() = 0
+        Catch ex As Exception
+            alert("Data entered fail, please Try again.")
+        End Try
     End Sub
-
-    Protected Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
-        SQL(0) = "GET_STUDENT; "
-        DT = M1.GetDatatable(SQL(0))
-    End Sub
-
-    Protected Sub btnSearchCancel_Click(sender As Object, e As EventArgs) Handles btnSearchCancel.Click
-
-    End Sub
-
-    '--แสดง ddlid กับ rb_status ใน gridview
-    'Protected Sub gridviewcompany_rowdatabound(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewRowEventArgs) Handles gridviewcompany.rowdatabound
-    '    If e.Row.RowType = DataControlRowType.DataRow Then
-    '        Dim drv As DataRowView = e.Row.DataItem
-    '        Dim no As Integer = e.Row.RowIndex
-    '        Dim itemno As Integer = (ViewState("page") * 10) + no
-
-    '        Dim ddlid As DropDownList = e.Row.FindControl("ddlid")
-    '        If Not IsNothing(ddlid) Then
-    '            Sql(0) = "select id_company,company_code ,company_code + ' , ' + company_namet as companyname from hrms.dbo.com_company"
-    '            dt1 = m1.getdatatable(Sql(0))
-    '            ddlid.DataSource = dt1
-    '            ddlid.DataTextField = "companyname"
-    '            ddlid.DataValueField = "id_company"
-    '            ddlid.DataBind()
-    '            ddlid.SelectedValue = dt.rows.item(itemno)("id_company").tostring
-    '        End If
-
-    '        Dim ddlholdingid As DropDownList = e.Row.FindControl("ddlholdingid")
-    '        If Not IsNothing(ddlholdingid) Then
-    '            ddlholdingid.SelectedValue = dt.rows.item(itemno)("holding_group").tostring
-    '        End If
-
-    '        Dim rb_status As RadioButtonList = e.Row.FindControl("rb_status")
-    '        If Not IsNothing(rb_status) Then
-    '            rb_status.SelectedValue = dt.rows.item(itemno)("status").tostring
-    '        End If
-    '    End If
-    'End Sub
 
 #End Region
 
 #Region "gridview"
 
-    ''--การจัดการภายใน gridview
-    'Protected Sub gridviewcostcenter_rowcancelingedit(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewCancelEditEventArgs) Handles gridviewcompany.rowcancelingedit
-    '    gridviewcompany.editindex = -1
-    '    Me.loaddata()
-    'End Sub
+    '--การจัดการภายใน gridview
+    Protected Sub gvStudent_rowcancelingedit(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewCancelEditEventArgs) Handles gvStudent.RowCancelingEdit
+        gvStudent.EditIndex = -1
+        Me.loaddata()
+    End Sub
 
-    'Protected Sub gridviewcompany_rowediting(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewEditEventArgs) Handles gridviewcompany.rowediting
-    '    gridviewcompany.editindex = e.NewEditIndex
-    '    bind Data to the gridview control.
-    '    Me.loaddata()
-    'End Sub
+    '--แสดง ddlID กับ rb_status ใน GridView
+    Protected Sub gvStudent_RowDataBound(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewRowEventArgs) Handles gvStudent.RowDataBound
+        If e.Row.RowType = DataControlRowType.DataRow Then
+            Dim drv As DataRowView = e.Row.DataItem
+            Dim no As Integer = e.Row.RowIndex
+            Dim itemNo As Integer = (ViewState("Page") * 10) + no
 
-    'Protected Sub gridviewcompany_rowupdating(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewUpdateEventArgs) Handles gridviewcompany.rowupdating
-    '    Dim index As Integer = e.RowIndex
-    '    Dim compcode As String = Me.gridviewcompany.datakeys(index).values(0).tostring()
-    '    Dim compname As TextBox = CType(gridviewcompany.rows(e.RowIndex).findcontrol("txtcomname"), TextBox)
-    '    Dim comp As DropDownList = CType(gridviewcompany.rows(e.RowIndex).findcontrol("ddlid"), DropDownList)
-    '    Dim rb_status As RadioButtonList = CType(gridviewcompany.rows(e.RowIndex).findcontrol("rb_status"), RadioButtonList)
+            Dim ddlID As DropDownList = e.Row.FindControl("ddlStuLevel")
+            If Not IsNothing(ddlID) Then
+                ddlID.SelectedValue = DT.Rows.Item(itemNo)("stulevel").ToString
+            End If
+        End If
+    End Sub
 
-    '    Sql(0) = "update master_company set comname = '" & compname.Text & "', id_company = '" & comp.SelectedValue.ToString() & "' , status = '" & rb_status.SelectedValue.ToString() & "' where comcode = '" & compcode & "'"
-    '    m1.execute(Sql(0))
-    '    alert("ดำเนินการเรียบร้อยแล้วค่ะ\ndata is complete.")
-    '    gridviewcompany.editindex = -1
-    '    loaddata()
 
-    'End Sub
+    Protected Sub gvStudent_rowediting(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewEditEventArgs) Handles gvStudent.RowEditing
+        gvStudent.EditIndex = e.NewEditIndex
+        'bind Data to the gridview control.
+        Me.loaddata()
+    End Sub
 
-    'Protected Sub gridviewcompany_selectedindexchanging(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewSelectEventArgs) Handles gridviewcompany.selectedindexchanging
-    '    Dim k1 As DataKey = gridviewcompany.datakeys(e.NewSelectedIndex)
-    'End Sub
+    Protected Sub gvStudent_rowupdating(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewUpdateEventArgs) Handles gvStudent.RowUpdating
+        Dim index As Integer = e.RowIndex
+        Dim stuid As String = Me.gvStudent.DataKeys(index).Values(0).ToString()
+        Dim stuname As TextBox = CType(gvStudent.Rows(e.RowIndex).FindControl("txtStuName"), TextBox)
+        Dim stulevel As DropDownList = CType(gvStudent.Rows(e.RowIndex).FindControl("ddlStuLevel"), DropDownList)
 
-    'Protected Sub gridviewcompany_pageindexchanging(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewPageEventArgs) Handles gridviewcompany.pageindexchanging
-    '    Me.gridviewcompany.pageindex = e.NewPageIndex
-    '    ViewState("page") = Me.gridviewcompany.pageindex
-    '    loaddata()
-    'End Sub
+        Dim stunameStr As String = stuname.Text
+        'stuLevel = '" & stulevel.SelectedValue.ToString() & "' " 
+
+        Dim str As String = stuname.Text
+
+        SQL(0) = " update student set stuname = '" & stunameStr & "' where stuId = " & stuid & " "
+        M1.Execute(SQL(0))
+        alert("Data edited successfully")
+        gvStudent.EditIndex = -1
+        loaddata()
+
+    End Sub
+
+    Protected Sub gridviewcompany_selectedindexchanging(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewSelectEventArgs) Handles gvStudent.SelectedIndexChanging
+        Dim k1 As DataKey = gvStudent.DataKeys(e.NewSelectedIndex)
+    End Sub
+
+    Protected Sub gridviewcompany_pageindexchanging(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewPageEventArgs) Handles gvStudent.PageIndexChanging
+        Me.gvStudent.PageIndex = e.NewPageIndex
+        ViewState("page") = Me.gvStudent.PageIndex
+        loaddata()
+    End Sub
 
 #End Region
 
 #Region "search"
 
-    'Protected Sub chkholding_checkedchanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles chkholding.checkedchanged
-    '    ddlsearchholding.enabled = True
-    '    chkcomcode.checked = False
-    '    txtsearchcomcode.enabled = False
-    'End Sub
+    Protected Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        'SQL(0) = "GET_STUDENT; "
+        SQL(0) = "Select * From Student" _
+                & " Where stuid Like '%" & txtSearch.Text & "%' " _
+                & " Or stuname like '%" & txtSearch.Text & "%'  "
+        DT = M1.GetDatatable(SQL(0))
+        gvStudent.DataSource = DT
+        gvStudent.DataBind()
+    End Sub
 
-    'Protected Sub chkcomcode_checkedchanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles chkcomcode.checkedchanged
-    '    txtsearchcomcode.enabled = True
-    '    chkholding.checked = False
-    '    ddlsearchholding.enabled = False
-    'End Sub
-
-    'Protected Sub btnsearch_click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSearch.Click
-    '    If chksearch() = False Then
-    '        Exit Sub
-    '    End If
-
-    '    If chkholding.checked = True Then
-    '        Dim holding As String = ddlsearchholding.selectedvalue.tostring()
-    '        Sql(0) = " select * , case when a.status = 'y' then 'use' else 'not use' end as status1 ," _
-    '           & " case when a.holding_group = 'aa' then 'aa : paper holding' when a.holding_group = 'po' then 'po : power holding'  else 'qs : other holding' end as holding , " _
-    '           & " company_code + ' , ' + b.company_namet as companyname from [true].[dbo].[master_company] a" _
-    '           & " join [hrms].[dbo].[com_company] b on a.id_company = b.id_company " _
-    '           & " where a.holding_group = '" & holding & "' order by holding_group asc, comcode asc "
-    '        dt = m1.getdatatable(Sql(0))
-    '        gridviewcompany.datasource = dt
-    '        gridviewcompany.databind()
-    '    Else
-    '        Dim comcode As String = txtsearchcomcode.text
-    '        Sql(0) = " select * , case when a.status = 'y' then 'use' else 'not use' end as status1 ," _
-    '           & " case when a.holding_group = 'aa' then 'aa : paper holding' when a.holding_group = 'po' then 'po : power holding'  else 'qs : other holding' end as holding , " _
-    '           & " company_code + ' , ' + b.company_namet as companyname from [true].[dbo].[master_company] a" _
-    '           & " join [hrms].[dbo].[com_company] b on a.id_company = b.id_company " _
-    '           & " where a.comcode = '" & comcode & "' order by holding_group asc, comcode asc "
-    '        dt = m1.getdatatable(Sql(0))
-    '        gridviewcompany.datasource = dt
-    '        gridviewcompany.databind()
-    '    End If
-    'End Sub
-
-    'Protected Sub btnsearchcancel_click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSearchCancel.Click
-    '    loaddata()
-    '    chkholding.checked = False
-    '    chkcomcode.checked = False
-    '    ddlsearchholding.enabled = False
-    '    txtsearchcomcode.enabled = False
-    'End Sub
+    Protected Sub btnSearchCancel_Click(sender As Object, e As EventArgs) Handles btnSearchCancel.Click
+        txtSearch.Text = ""
+        loaddata()
+    End Sub
 
 #End Region
 
